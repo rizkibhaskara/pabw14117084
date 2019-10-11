2a3b62f9bf3c00dda2fdfb4dc2b1af9fd88162af
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2019-10-11 10:57:28 +0700

    Add Tugas Awal Boleh Pulang

---
+++ Week-6/Latihan 1/Latihan1.5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <title>Contoh Skrip PHP</title>
+
+<body>
+
+
+
+<?php
+  $i =2;
+  do{
+    echo "\$i = $i <br>";
+    $i++;
+  } while ($i < 5);
+?>
+
+</body>
+</html>
