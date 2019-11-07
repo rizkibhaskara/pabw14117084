6ef620b31ffbb1d1a153275ae88dfc87c25dc33a
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2019-11-07 08:16:27 +0700

    Add

--- Week-8/Tugas/latihand.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$cari=$_POST['dnama'];
-$conn=mysql_connect("localhost","root","");
-mysql_selectdb("itera",$conn);
-$hasil=mysql_query("delete from mahasiswa where nama like '%$cari%'",$conn);
-if ($hasil) {
-	echo "DATA MAHASISWA BERHASIL DIHAPUS";
-}
- ?>
